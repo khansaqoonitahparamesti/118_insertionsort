18e83b85bad5c5ebc90784e818588233056b1daa
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-03-15 11:17:15 +0700

    deklarasikan variabel

--- 118_insertionsort/118_insertionsort.cpp
+++ 118_insertionsort/118_insertionsort.cpp
@@ -26,6 +26,20 @@ void input() {  // prosedur untuk input
 	}
 }
 
+void insertionsort() {
+	int i, n, temp, j;
+	for (i = 1; i <= n - 1; i++) //step 1
+	{
+		temp = arr[i];//step 2
+		j = i - 1; //step 3
+		while (j >= 0 && arr[j] > temp) // step 4
+		{
+			arr[j + 1] = arr[j];//step 4a
+			j = j - 1;//step 4b
+		}
+	}   arr[j + 1] = temp;//step 5
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
