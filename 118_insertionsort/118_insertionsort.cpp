f7ff5032ce98a864386896b6f26550439c5524e3
khansaqoonitahparamesti <khansaqoonita@gmail.com>
2024-03-15 11:18:13 +0700

    masukan display

--- 118_insertionsort/118_insertionsort.cpp
+++ 118_insertionsort/118_insertionsort.cpp
@@ -39,6 +39,17 @@ void insertionsort() {
 		}
 	}   arr[j + 1] = temp;//step 5
 }
+void display() {
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "element Array yang telah tersusun" << endl;
+	cout << "=====================" << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << "jumalah pass =" << n - 1 << endl;
+	cout << endl;
+}
 
 int main()
 {
